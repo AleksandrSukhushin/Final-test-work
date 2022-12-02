2afc6d2a4236fa2a70b0ea60a07977fa9935f94b
Alexander <asukhushin@icloud.com>
2022-12-02 22:38:12 +0300

    Решение задачи

--- Verification work/Program.cs
+++ Verification work/Program.cs
@@ -7,3 +7,39 @@
 // ["hello", "2", "world", ":-)" -> ["2", ";-)"]
 // ["1234", "1567", "-2", "comruter science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> [ ]
+
+Console.Clear();
+
+Console.Write("Количество элекментов исходного массива: ");
+int amount = Convert.ToInt32(Console.ReadLine()!);
+
+string[] arrayInitial = new string[amount];
+for (int i = 0; i < amount; i++)
+{
+    Console.Write($"Введите элемент {i+1}: ");
+    string element = Convert.ToString(Console.ReadLine()!);
+    arrayInitial[i] = element;
+}
+
+string[] arrayFinal = new string[amount];
+int k = 3;
+int index = 0;
+for (int j = 0; j < amount; j++)
+{
+    if (arrayInitial[j].Length <= k)
+    {
+        arrayFinal[index] = arrayInitial[j];
+        index++;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+PrintArray(arrayFinal);
